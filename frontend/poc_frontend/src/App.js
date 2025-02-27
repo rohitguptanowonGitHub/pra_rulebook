e155aa476b28573e2f149a8088b0eedfa82d0106
Rohit Gupta <rohit.gupta3@in.ey.com>
2025-02-27 13:08:17 +0530

    UI changes

--- frontend/poc_frontend/src/App.js
+++ frontend/poc_frontend/src/App.js
@@ -4,15 +4,15 @@ import "./App.css";
 const  data = {
   "COR001 Own Funds": {
     "schedules": {
-      "1 - Schedule": ["Dummy data for 1 - Schedule"],
-      "2 - Schedule": ["Dummy data for 2 - Schedule"],
-      "3 - Schedule": ["Dummy data for 3 - Schedule"],
-      "4 - Schedule": ["Dummy data for 4 - Schedule"],
-      "5.1 - Schedule": ["Dummy data for 5.1 - Schedule"],
-      "5.2 - Schedule": ["Dummy data for 5.2 - Schedule"],
-      "6.1 - Schedule": ["Dummy data for 6.1 - Schedule"],
-      "6.2 - Schedule": ["Dummy data for 6.2 - Schedule"],
-      "7 - Schedule": ["SA Exposure class",
+      "C 01.00 - OWN FUNDS": ["Dummy data for 1 - Schedule"],
+      "C 02.00 - OWN FUNDS REQUIREMENTS": ["Dummy data for 2 - Schedule"],
+      "C 03.00 - CAPITAL RATIOS": ["Dummy data for 3 - Schedule"],
+      "C 04.00 - MEMORANDUM ITEMS": ["Dummy data for 4 - Schedule"],
+      "C 05.01 - TRANSITIONAL PROVISIONS": ["Dummy data for 5.1 - Schedule"],
+      "C 05.02 - GRANDFATHERED INSTRUMENTS(CA5.2)": ["Dummy data for 5.2 - Schedule"],
+      "C 06.01 - GROUP SOLVENCY-TOTAL": ["Dummy data for 6.1 - Schedule"],
+      "C 06.02 - GROUP SOLVENCY": ["Dummy data for 6.2 - Schedule"],
+      "C 07.00 - CR SA(CREDIT AND COUNTERPARTY CREDIT RISKS AND FREE DELIVERIES)": ["SA Exposure class",
         "TOTAL EXPOSURES",
         "of which: Defaulted exposures",
         "of which: SME",
@@ -35,53 +35,53 @@ const  data = {
         "Exposures in default subject to a risk weight of 100%",
         "Exposures secured by mortgages on residential property",
         "Exposures in default subject to a risk weight of 150%"],
-      "8.1 - Schedule": ["Dummy data for 8.1 - Schedule"],
-      "8.2 - Schedule": ["Dummy data for 8.2 - Schedule"],
-      "8.3 - Schedule": ["Dummy data for 8.3 - Schedule"],
-      "8.4 - Schedule": ["Dummy data for 8.4 - Schedule"],
-      "8.5 - Schedule": ["Dummy data for 8.5 - Schedule"],
-      "8.5.1 - Schedule": ["Dummy data for 8.5.1 - Schedule"],
-      "8.6 - Schedule": ["Dummy data for 8.6 - Schedule"],
-      "8.7 - Schedule": ["Dummy data for 8.7 - Schedule"],
-      "9.1 - Schedule": ["Dummy data for 9.1 - Schedule"],
-      "9.2 - Schedule": ["Dummy data for 9.2 - Schedule"],
-      "9.4 - Schedule": ["Dummy data for 9.4 - Schedule"],
-      "10.1 - Schedule": ["Dummy data for 10.1 - Schedule"],
-      "10.2 - Schedule": ["Dummy data for 10.2 - Schedule"],
-      "11 - Schedule": ["Dummy data for 11 - Schedule"],
-      "13.1 - Schedule": ["Dummy data for 13.1 - Schedule"],
-      "14 - Schedule": ["Dummy data for 14 - Schedule"],
-      "14.1 - Schedule": ["Dummy data for 14.1 - Schedule"],
-      "34.1 - Schedule": ["Dummy data for 34.1 - Schedule"],
-      "34.2 - Schedule": ["Dummy data for 34.2 - Schedule"],
-      "34.3 - Schedule": ["Dummy data for 34.3 - Schedule"],
-      "34.4 - Schedule": ["Dummy data for 34.4 - Schedule"],
-      "34.5 - Schedule": ["Dummy data for 34.5 - Schedule"],
-      "34.6 - Schedule": ["Dummy data for 34.6 - Schedule"],
-      "34.7 - Schedule": ["Dummy data for 34.7 - Schedule"],
-      "34.8 - Schedule": ["Dummy data for 34.8 - Schedule"],
-      "34.9 - Schedule": ["Dummy data for 34.9 - Schedule"],
-      "34.10 - Schedule": ["Dummy data for 34.10 - Schedule"],
-      "34.11 - Schedule": ["Dummy data for 34.11 - Schedule"],
-      "16 - Schedule": ["Dummy data for 16 - Schedule"],
-      "17.1 - Schedule": ["Dummy data for 17.1 - Schedule"],
-      "17.2 - Schedule": ["Dummy data for 17.2 - Schedule"],
-      "18 - Schedule": ["Dummy data for 18 - Schedule"],
-      "19 - Schedule": ["Dummy data for 19 - Schedule"],
-      "20 - Schedule": ["Dummy data for 20 - Schedule"],
-      "21 - Schedule": ["Dummy data for 21 - Schedule"],
-      "22 - Schedule": ["Dummy data for 22 - Schedule"],
-      "23 - Schedule": ["Dummy data for 23 - Schedule"],
-      "24 - Schedule": ["Dummy data for 24 - Schedule"],
-      "25 - Schedule": ["Dummy data for 25 - Schedule"],
-      "32.1 - Schedule": ["Dummy data for 32.1 - Schedule"],
-      "32.2 - Schedule": ["Dummy data for 32.2 - Schedule"],
-      "32.3 - Schedule": ["Dummy data for 32.3 - Schedule"],
-      "32.4 - Schedule": ["Dummy data for 32.4 - Schedule"],
-      "33 - Schedule": ["Dummy data for 33 - Schedule"],
-      "35.1 - Schedule": ["Dummy data for 35.1 - Schedule"],
-      "35.2 - Schedule": ["Dummy data for 35.2 - Schedule"],
-      "35.3 - Schedule": ["Dummy data for 35.3 - Schedule"],
+      "C 08.01 - CR IRB1(CREDIT AND COUNTERPARTY CREDIT RISKS AND FREE DELIVERIES)": ["Dummy data for 8.1 - Schedule"],
+      "C 08.02 - CR IRB2(CREDIT AND COUNTERPARTY CREDIT RISKS AND FREE DELIVERIES)": ["Dummy data for 8.2 - Schedule"],
+      "C 08.03 - CR IRB3(CREDIT RISK AND FREE DELIVERIES)": ["Dummy data for 8.3 - Schedule"],
+      "C 08.04 - CR IRB4(CREDIT RISK AND FREE DELIVERIES)": ["Dummy data for 8.4 - Schedule"],
+      "C 08.05 - CR IRB5(CREDIT RISK AND FREE DELIVERIES)": ["Dummy data for 8.5 - Schedule"],
+      "C 08.05.1- CREDIT RISK AND FREE DELIVERIES": ["Dummy data for 8.5.1 - Schedule"],
+      "C 08.06 - CR IRB6(CREDIT RISK AND FREE DELIVERIES)": ["Dummy data for 8.6 - Schedule"],
+      "C 08.07 - CR IRB7(CREDIT RISK AND FREE DELIVERIES)": ["Dummy data for 8.7 - Schedule"],
+      "C 09.01 - CR GB 1(Table 9.1 - Geographical breakdown of exposures by residence of the obligor)": ["Dummy data for 9.1 - Schedule"],
+      "C 09.02 - CR GB 2(Table 9.2 - Geographical breakdown of exposures by residence of the obligor)": ["Dummy data for 9.2 - Schedule"],
+      "C 09.04 - CCB(Table 9.4 - Breakdown of credit exposures relevant for the calculation of the countercyclical buffer )": ["Dummy data for 9.4 - Schedule"],
+      "C 10.01 - CR EQU IRB 1(CREDIT RISK)": ["Dummy data for 10.1 - Schedule"],
+      "C 10.02 - CR EQU IRB 2(CREDIT RISk)": ["Dummy data for 10.2 - Schedule"],
+      "C 11.00 - SETTLEMENT/DELIVERY RISK": ["Dummy data for 11 - Schedule"],
+      "C 13.01 - CREDIT RISK: SECURITISATIONS": ["Dummy data for 13.1 - Schedule"],
+      "C 14.00 - DETAILED INFORMATION ON SECURITISATIONS": ["Dummy data for 14 - Schedule"],
+      "C 14.01 - DETAILED INFORMATION ON SECURITISATIONS BY APPROACH ": ["Dummy data for 14.1 - Schedule"],
+      "C 34.01 - CCR 1(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.1 - Schedule"],
+      "C 34.02 - CCR 2(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.2 - Schedule"],
+      "C 34.03 - CCR 3(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.3 - Schedule"],
+      "C 34.04 - CCR 4(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.4 - Schedule"],
+      "C 34.05 - CCR 5(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.5 - Schedule"],
+      "C 34.06 - CCR 6(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.6 - Schedule"],
+      "C 34.07 - CCR 7(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.7 - Schedule"],
+      "C 34.08 - CCR 8(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.8 - Schedule"],
+      "C 34.09 - CCR 9(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.9 - Schedule"],
+      "C 34.10 - CCR 10(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.10 - Schedule"],
+      "C 34.11 - CCR 11(COUNTERPARTY CREDIT RISK)": ["Dummy data for 34.11 - Schedule"],
+      "C 16.00 - OPERATIONAL RISK": ["Dummy data for 16 - Schedule"],
+      "C 17.01 - OPERATIONAL RISK": ["Dummy data for 17.1 - Schedule"],
+      "C 17.02 - OPERATIONAL RISK": ["Dummy data for 17.2 - Schedule"],
+      "C 18.00 - MKR SA TDI(MARKET RISK)": ["Dummy data for 18 - Schedule"],
+      "C 19.00 - MKR SA SEC(MARKET RISK)": ["Dummy data for 19 - Schedule"],
+      "C 20.00 - MKR SA CTP(MARKET RISK)": ["Dummy data for 20 - Schedule"],
+      "C 21.00 - MKR SA EQU(MARKET RISK)": ["Dummy data for 21 - Schedule"],
+      "C 22.00 - MKR SA FX(MARKET RISK)": ["Dummy data for 22 - Schedule"],
+      "C 23.00 - MKR SA COM(MARKET RISK)": ["Dummy data for 23 - Schedule"],
+      "C 24.00 - MKR IM(MARKET RISK INTERNAL MODELS)": ["Dummy data for 24 - Schedule"],
+      "C 25.00 - CREDIT VALUE ADJUSTMENT RISK": ["Dummy data for 25 - Schedule"],
+      "C 32.01 - PRUDENT VALUATION: FAIR-VALUED ASSETS AND LIABILITIES": ["Dummy data for 32.1 - Schedule"],
+      "C 32.02 - PRUDENT VALUATION: CORE APPROACH": ["Dummy data for 32.2 - Schedule"],
+      "C 32.03 - PRUDENT VALUATION: MODEL RISK AVA": ["Dummy data for 32.3 - Schedule"],
+      "C 32.04 - PRUDENT VALUATION: CONCENTRATED POSITIONS AVA": ["Dummy data for 32.4 - Schedule"],
+      "C 33.00 - GENERAL GOVERNMENTS EXPOSURES BY COUNTRY OF THE COUNTERPARTY": ["Dummy data for 33 - Schedule"],
+      "C 35.01 - NPE LC1(NPE LOSS COVERAGE)": ["Dummy data for 35.1 - Schedule"],
+      "C 35.02 - NPE LC2(NPE LOSS COVERAGE)": ["Dummy data for 35.2 - Schedule"],
+      "C 35.03 - NPE LC3(NPE LOSS COVERAGE)": ["Dummy data for 35.3 - Schedule"],
     },
   },
   "COR002 Market Risk": {
@@ -186,6 +186,7 @@ function App() {
   const [isInterpreting, setIsInterpreting] = useState(false);
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false); // Loading state
+  const [warning, setWarning] = useState("");
 
   async function getInterpretation(selectedItem) {
     await fetch(`http://127.0.0.1:8000/process?input_value=${selectedItem}`, {
@@ -200,24 +201,27 @@ function App() {
       .then((data) => {
         setInterpretation(data.output);
         setArticles(data.articles || []);
-        console.log(data.output);
+        setWarning(data.warning)
+        console.log(data);
       });
   }
 useEffect(() => {
     if (isInterpreting) {
-      const updatedSchedule = `${selectedReturn} || ${selectedSchedule} || ${selectedItem}`;
       setTableData((prevData) => [
         ...prevData,
         { 
-          returnId: updatedSchedule, 
-          schedule: articles, // Join articles with newline
-          item: interpretation 
+          returnId: selectedReturn, 
+          schedule: selectedSchedule, // Join articles with newline
+          item: selectedItem,
+          articles: articles,
+          interpretation: interpretation,
+          warning : warning
         },
       ]);
       setIsInterpreting(false);
       setIsLoading(false);
     }
-  }, [interpretation,articles]);
+  }, [interpretation,articles,warning]);
   // When Return is selected, auto-update Schedule & Item
   const handleReturnChange = (event) => {
     const newReturn = event.target.value;
@@ -326,23 +330,27 @@ useEffect(() => {
         )  : (<table className="data-table">
           <thead>
             <tr>
-              <th>Title || Schedule || Item</th>
-              <th>Articles</th>
-              <th>Interpretation</th>
+              <th className = "title" >Title</th>
+              <th className = "schedule">Schedule</th>
+              <th className = "item">Item</th>
+              <th className = "articles-h">Articles</th>
+              <th className = "interpretation-h ">Interpretation</th>
             </tr>
           </thead>
           <tbody>
             {tableData.length > 0 ? (
               tableData.map((row, index) => (
                 <tr key={index}>
-                  <td className="tsi">{row.returnId}</td>
-                  <td className="article">{row.schedule}</td>
-                  <td className="interpretation">{row.item}</td>
+                  <td className="title">{row.returnId}</td>
+                  <td className="schedule">{row.schedule}</td>
+                  <td className="item">{row.item}</td>
+                  <td className="articles">{row.articles}</td>
+                  <td className="interpretation"><tr className="interpretation-content">{row.interpretation}</tr><tr className="warning" >{row.warning}</tr></td>
                 </tr>
               ))
             ) : (
               <tr>
-                <td colSpan="3">No data selected yet.</td>
+                <td colSpan="5">No data selected yet.</td>
               </tr>
             )}
           </tbody>
